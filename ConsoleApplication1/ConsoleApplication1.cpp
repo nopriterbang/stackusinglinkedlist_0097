9975e11c0c2ede23f78911136ad0384bc26343c0
nopriterbang <novalpriya@gmail.com>
2024-05-10 16:50:08 +0700

    menambahkan perulangan, tampilan didalam void peek

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -43,7 +43,17 @@ public:
 	}
 
 	void peek() {
-
+		if (top == NULL) {
+			cout << "List is empty. " << endl;
+		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << "" << endl;
+				current = current->next;
+			}
+			cout << endl;
+		}
 	}
 
 };
