138caa3b755245ed9d2a8dfb766f5eba7078cb62
nopriterbang <novalpriya@gmail.com>
2024-05-10 17:01:59 +0700

    membuat case 1

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -78,6 +78,15 @@ int main() {
 		cin >> choice;
 
 		switch (choice) {
+		case 1:
+			cout << "Enter the value to push: ";
+			cin >> value;
+			stack.push(value);
+			break;
+		
+		
+		}
+
 	}
 	return 0;
 }
